dad1c248a53d412469ca19c6b21c27e0d862c38c
Tim Krämer <kraemerstim@gmail.com>
2017-10-07 20:35:29 +0200

    random Sounds implementiert

--- raspberryPi/soundMode.py
+++ raspberryPi/soundMode.py
@@ -5,6 +5,8 @@ import FB_Status
 import feeling_rest
 from datetime import datetime, timedelta
 from pygame import mixer
+import os
+from random import randint
 
 class Sound_Mode:
   
@@ -19,12 +21,21 @@ class Sound_Mode:
     self.sounds.append('sounds/timetogetlaid.mp3')
     self.sounds.append('sounds/justhadsex.mp3')
     self.sounds.append('sounds/incrediblethoughts.mp3')
+    
+    self.randomSounds = []
+    for root, dirs, files in os.walk('sounds/random/'):
+      for filename in files:
+        self.randomSounds.append('sounds/random/' + filename)
   
   def ButtonPressed(self, button):
-    mixer.music.load(self.sounds[button])
+    if (button < len(self.sounds)):
+      sound = self.sounds[button]
+    else:
+      sound = self.randomSounds[randint(0, len(self.randomSounds)-1)]
+      
+    mixer.music.load(sound)
+    self.display.setDisplay('Now Playing', sound)
     mixer.music.play()
-    self.display.setDisplay('Now Playing', self.sounds[button])
-    
   
   def HotButtonPressed(self):
     self.status.resetMode()
