93ebf9bb42afccbac1866f27ef92fa29de86415a
Tim KrÃ¤mer <kraemerstim@gmail.com>
2018-03-17 09:54:03 +0100

    Initial commit Milestones

---
+++ springBoot/src/main/java/de/tim/feeling/milestone/Milestone.java
@@ -0,0 +1,65 @@
+package de.tim.feeling.milestone;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import de.tim.feeling.account.Account;;
+
+@Entity
+public class Milestone {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	private String description;
+	private Timestamp timestamp;
+	
+	@ManyToOne
+	private Account account;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+}
---
+++ springBoot/src/main/java/de/tim/feeling/milestone/MilestoneRepository.java
@@ -0,0 +1,6 @@
+package de.tim.feeling.milestone;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MilestoneRepository extends CrudRepository<Milestone, Long> {
+}
